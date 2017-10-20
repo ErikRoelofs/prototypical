bbfa4ca82b91e37c864e07f76bb648d7a861776a
Erik Roelofs <erik@ruigekonijnen.nl>
2017-10-20 22:25:20 +0200

    More cleanup.

--- creator/entityCreator.py
+++ creator/entityCreator.py
@@ -1,4 +1,9 @@
 import random
+from domain.token import Token
+from domain.die import Die
+from tts.blocksquare import BlockSquare
+from tts.transform import Transform
+from tts.die import Die as TTSDie
 
 XMIN = -27
 XMAX = 27
@@ -7,11 +12,11 @@ ZMAX = 17
 YHEIGHT = 1.5
 
 
-class EntityPlacer:
+class EntityCreator:
     def __init__(self, library):
         self.library = library
 
-    def getCoordInChunk(chunkX, chunkY, numXChunks, numYChunks):
+    def getCoordInChunk(self, chunkX, chunkY, numXChunks, numYChunks):
         width = (XMAX - XMIN) / numXChunks
         height = (ZMAX - ZMIN) / numYChunks
         xOffset = random.uniform(0, width)
@@ -23,12 +28,26 @@ class EntityPlacer:
             if type.name == name:
                 return type
 
-    def createEntity(self, entity, location):
+    def createEntity(self, coords, entity):
+        if isinstance(entity, Token):
+            return self.placeToken(coords, entity)
+        if isinstance(entity, Die):
+            return self.placeDie(coords, entity)
+
+    def placeToken(self, coords, entity):
+        transform = Transform(coords[0], YHEIGHT, coords[1], 0, 0, 0, entity.size, entity.size, entity.size)
+        bs = BlockSquare(transform, entity.color)
+        return bs.as_dict()
+
+    def placeDie(self, coords, entity):
+        transform = Transform(coords[0], YHEIGHT, coords[1], 0, 0, 0, entity.size, entity.size, entity.size)
+        die = TTSDie(entity.sides, entity.color, transform)
+        return die.as_dict()
 
     def createEntities(self, sheet):
         chunksWide = sheet.cell(rowx=0, colx=1).value
         chunksHigh = sheet.cell(rowx=0, colx=2).value
-
+        entities = []
         row = 1
         while row < sheet.nrows:
             xChunk = sheet.cell(rowx=row, colx=0).value
@@ -40,6 +59,7 @@ class EntityPlacer:
                     typeToPlace = sheet.cell(rowx=row, colx=col + 1).value
                     object = self.findObjectByName(typeToPlace)
                     for i in range(0, int(numToPlace)):
-                        self.createEntity(self.getCoordInChunk(xChunk, yChunk, chunksWide, chunksHigh), object)
+                        entities.append(self.createEntity(self.getCoordInChunk(xChunk, yChunk, chunksWide, chunksHigh), object))
                 col += 2
             row += 1
+        return entities
--- script.py
+++ script.py
@@ -1,59 +1,10 @@
-XMIN = -27
-XMAX = 27
-ZMIN = -17
-ZMAX = 17
-YHEIGHT = 1.5
+import json, xlrd
 
-import json, random, xlrd,sys
-from tts.blocksquare import BlockSquare
-from tts.transform import Transform
-from tts.die import Die as TTSDie
-
-from domain.token import Token
-from domain.die import Die
 from sheetParser.tokenParser import TokenParser
 from sheetParser.diceParser import DiceParser
 
-def findObjectByName(name):
-    maybe = findTokenByName(name)
-    if maybe:
-        return maybe
-    maybe = findDieByName(name)
-    if maybe:
-        return maybe
+from creator.entityCreator import EntityCreator
 
-def findTokenByName(name):
-    for token in tokens:
-        if token.name == name:
-            return token
-
-def findDieByName(name):
-    for die in dice:
-        if die.name == name:
-            return die
-
-def placeEntity(coords,entity):
-    if isinstance(entity, Token):
-        placeToken(coords, entity)
-    if isinstance(entity, Die):
-        placeDie(coords, entity)
-
-def placeToken(coords,entity):
-    transform = Transform(coords[0], YHEIGHT, coords[1], 0, 0, 0, entity.size, entity.size, entity.size)
-    bs = BlockSquare(transform, entity.color)
-    data["ObjectStates"].append(bs.as_dict())
-
-def placeDie(coords,entity):
-    transform = Transform(coords[0], YHEIGHT, coords[1], 0, 0, 0, entity.size, entity.size, entity.size)
-    die = TTSDie(entity.sides, entity.color, transform)
-    data["ObjectStates"].append(die.as_dict())
-
-def getCoordInChunk(chunkX, chunkY, numXChunks, numYChunks):
-    width = (XMAX - XMIN) / numXChunks
-    height = (ZMAX - ZMIN) / numYChunks
-    xOffset = random.uniform(0, width)
-    yOffset = random.uniform(0, height)
-    return ( xOffset + XMIN + (chunkX * width), yOffset + ZMIN + (chunkY * height))
 
 # open save template
 with open('template.json', 'r') as infile:
@@ -62,28 +13,17 @@ with open('template.json', 'r') as infile:
 # open excel file
 workbook = xlrd.open_workbook('cubes.xls')
 
+# collect entity libraries
 tokens = TokenParser.parse(workbook.sheet_by_name('Tokens'))
 dice = DiceParser.parse(workbook.sheet_by_name('Dice'))
 
-# read item locations
-placementSheet = workbook.sheet_by_name('Placement')
-chunksWide = placementSheet.cell(rowx=0, colx=1).value
-chunksHigh = placementSheet.cell(rowx=0, colx=2).value
+# build all required entities
+creator = EntityCreator(tokens + dice)
+entities = creator.createEntities(workbook.sheet_by_name('Placement'))
 
-row = 1
-while row < placementSheet.nrows:
-    xChunk = placementSheet.cell(rowx=row, colx=0).value
-    yChunk = placementSheet.cell(rowx=row, colx=1).value
-    col = 2
-    while col < placementSheet.ncols and col < 10:
-        numToPlace = placementSheet.cell(rowx=row, colx=col).value
-        if numToPlace:
-            typeToPlace = placementSheet.cell(rowx=row, colx=col+1).value
-            token = findObjectByName(typeToPlace)
-            for i in range(0,int(numToPlace)):
-                placeEntity(getCoordInChunk(xChunk, yChunk, chunksWide, chunksHigh), token)
-        col += 2
-    row += 1
+# add entities to save file
+data["ObjectStates"] = entities
 
+# save file
 with open('TS_Save_3.json', 'w') as outfile:
     json.dump(data, outfile)
